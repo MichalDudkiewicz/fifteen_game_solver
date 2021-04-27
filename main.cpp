8adbafb1e0d70bd2f7d7d9081084647203d00e8a
Michal Dudkiewicz <michal.dudkiewicz@tomtom.com>
2021-04-28 00:32:26 +0200

    docs

--- main.cpp
+++ main.cpp
@@ -25,6 +25,18 @@ std::vector<Operation> parseOperations(const std::string& operations)
     return operationsParsed;
 }
 
+/*
+ * Program do rozwiązywania fifteen puzzle o dowolnym prostokątnym rozmiarze.
+ *
+ * Instrukcja obsługi programu oraz opis parametrów wywołania są dostępne tutaj: https://ftims.edu.p.lodz.pl/mod/page/view.php?id=59205
+ *
+ * Uwaga: ostatnie dwa parametry wywołania nie są obowiązkowe. Ich podanie nic nie zmienia w działaniu programu.
+ *
+ * Program należy uruchomić w katalogu, w którym znajduje się podkatalog o nazwie 'unsolved'.
+ * Z niego pobierane są układy w formie plików txt do rozwiązania. Układy rozwiązane są zapisywane w podkatalogu 'solved'
+ * (nie ma potrzeby tworzyć go ręcznie, jest on tworzony automatycznie).
+ *
+ */
 int main(int argc, char **argv) {
     if (argc < 4)
     {

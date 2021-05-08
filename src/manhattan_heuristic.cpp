43d9425601298b4cb911848bf2b54f85c2f4ef3b
Michal Dudkiewicz <michal.dudkiewicz@tomtom.com>
2021-05-08 16:08:40 +0200

    manhattan fixed

--- src/manhattan_heuristic.cpp
+++ src/manhattan_heuristic.cpp
@@ -4,7 +4,11 @@ namespace fifteen
 {
     unsigned int calculateMetricPartial(int rowIndex, int columnIndex, int value, int rowsNumber, int columnsNumber)
     {
-        return std::abs(value % rowsNumber + value % columnsNumber - 1 - rowIndex - columnIndex);
+        const int x2 = rowIndex;
+        const int y2 = columnIndex;
+        const int y1 = (value - 1) % columnsNumber;
+        const int x1 = (value - 1) / rowsNumber;
+        return std::abs(x2 - x1) + std::abs(y2 - y1);
     }
 
     unsigned int ManhattanHeuristic::estimateMinPathCostToTarget(const std::shared_ptr<Node> &node) const {
